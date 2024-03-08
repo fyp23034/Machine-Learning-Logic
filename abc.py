3e9fb56010c9aa40676d793a2e5f3b033eb5039d
Justin Yeung <78725881+justinygtc@users.noreply.github.com>
2024-03-08 20:30:08 +0800

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,8 @@
+import logic
+
+
+print(logic.emailCategory("65427c82d747ca686fa7382f"))
+
+logic.userNLR("I want emails related to job, interview, and offers to assign a higher importance rating")
+
+logic.addToWhiteList("abc@hku.hk")
